a85ef05da80aab71bd02bcf3b8505cf23a2692e9
marykeerthana2006-dotcom <marykeerthana2006@gmail.com>
2026-08-26 13:57:39 +0530

    Time: 0 ms (100%), Space: 12.4 MB (57.6%) - LeetHub

---
+++ 1668-maximum-repeating-substring/1668-maximum-repeating-substring.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxRepeating(self, sequence, word):
+        """
+        :type sequence: str
+        :type word: str
+        :rtype: int
+        """
+
+        k = 0
+        repeated_word = ""
+
+        while (repeated_word + word) in sequence:
+            repeated_word += word
+            k += 1
+
+        return k
