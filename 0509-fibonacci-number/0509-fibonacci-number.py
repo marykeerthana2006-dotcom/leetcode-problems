6dba7ce8ae26e259466cab0161bf525f3f496378
marykeerthana2006-dotcom <marykeerthana2006@gmail.com>
2026-08-27 11:37:01 +0530

    Time: 16 ms (66.17%), Space: 12.3 MB (54.5%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def fib(self, n):
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+
+        prev, curr = 0, 1
+        for i in range(2, n + 1):
+            prev, curr = curr, prev + curr
+
+        return curr
