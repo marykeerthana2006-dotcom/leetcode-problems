c0e0ec5fd6a02ae5455d277260f659358a78c6d4
marykeerthana2006-dotcom <marykeerthana2006@gmail.com>
2026-08-27 12:00:21 +0530

    Time: 0 ms (100%), Space: 12.4 MB (55.87%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def addDigits(self, num):
+
+        while num >= 10:
+            total = 0
+
+            while num > 0:
+                total = total + (num % 10)
+                num = num // 10
+
+            num = total
+
+        return num
